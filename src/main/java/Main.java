b2206830e271117e89d3b23ec822594945d63370
zuzikx3 <z.plauszta@gmail.com>
2020-04-21 15:17:34 +0200

    Added downloading database with words; Added 2-players mode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,32 @@
-import java.util.Scanner;
+import java.util.*;
+import java.net.*;
+import java.io.*;
 
 public class Main {
 
     public static Scanner sc = new Scanner(System.in);
+    public static List<String> dataBase;
 
     public static void main(String[] args) throws ToManyMistakesException {
+        final String path = "https://raw.githubusercontent.com/dwyl/english-words/master/words.txt";
+        Random random = new Random();
+        downloadDatabase(path);
+
+        System.out.println("HANGMAN");
+        System.out.println("Do you want play alone or with someone? (\"1\" or \"2\")");
+        String option = sc.nextLine();
+        while(!option.equals("1") && !option.equals("2")){
+            System.out.println("Do you want play alone or with someone? (\"1\" or \"2\")");
+            option = sc.nextLine();
+        }
 
         do {
-            String word = getWordToGuess();
+            String word;
+            if(option.equals("2")){
+                word = getWordToGuess();
+            } else{
+                word = dataBase.get(random.nextInt(dataBase.size()));
+            }
             Hangman hangman = new Hangman(word);
             clearScreen();
 
@@ -20,6 +39,31 @@ public class Main {
         } while (!"x".equals(sc.nextLine()));
         System.out.println("Goodbye");
         sc.close();
+    }
+
+    private static void downloadDatabase(String path) {
+        StringBuilder content = new StringBuilder();
+        try {
+            URL url = new URL(path);
+
+            URLConnection urlConnection = url.openConnection();
+
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream()));
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        dataBase = new ArrayList<>(Arrays.asList(content.toString().split("\n")));
+        System.out.println(dataBase);
     }
 
     private static void printStage(Hangman hangman) throws ToManyMistakesException {
@@ -38,7 +82,7 @@ public class Main {
     }
 
     private static void endGameScreen(Hangman hangman) {
-        if(hangman.getStatus().equals(Status.GUESSED)){
+        if (hangman.getStatus().equals(Status.GUESSED)) {
             System.out.println("Congratultion! You have just guessed a word: " + hangman.getWord());
         } else {
             System.out.println(hangman.buildHangman());
@@ -50,7 +94,7 @@ public class Main {
         int stateOfGuess = hangman.checkLetter(letter);
         if (stateOfGuess == 0) {
             System.out.println("Already used!");
-        } else if(stateOfGuess == -1){
+        } else if (stateOfGuess == -1) {
             System.out.println("Missed.");
         }
     }
