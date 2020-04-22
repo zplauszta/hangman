4672288099065c88ffdd3def724f30264aa3e529
bartek1470 <bartek1470@gmail.com>
2020-04-22 20:08:13 +0200

    Jar (#3)
    * Remove modules & standalone application
    
    * Add maven shade plugin.
    Remove standalone application description from README.md.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module hangman {
-    requires javafx.graphics;
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    exports pl.zuzu.ui.gui to javafx.graphics;
-    exports pl.zuzu.ui.gui.controllers to javafx.fxml;
-}
